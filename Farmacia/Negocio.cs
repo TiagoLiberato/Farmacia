b6f8ecb78b5955316acf969952be4ea14267c72b
Tiago Liberato <tiagolfsm@gmail.com>
2022-02-01 20:08:41 -0300

    initial commit

---
+++ Farmacia/Negocio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacia
+{
+    public class Negocio
+    {
+        public void ExibeReceita(Receita receita)
+        {
+            Console.WriteLine("Nome: " + receita.Nome);
+            Console.WriteLine("Endereco: " + receita.Endereco);
+            Console.WriteLine("Remedio: " + receita.Remedio.Nome);
+            Console.WriteLine("Quantidade: " + receita.Remedio.Quantidade);
+
+        }
+    }
+}
---
+++ Farmacia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Farmacia;
+
+namespace Farmacia
+{
+   public class Program
+    {
+        static void Main(string[] arg)
+        {
+            Negocio negocio = new Negocio();
+            Receita receita = new Receita();
+            receita.Nome = "Bebuxa";
+            receita.Endereco = "São João da Boa Vista";
+            
+            Remedio remedio = new Remedio();
+            remedio.Nome = "Gardenal";
+            remedio.Quantidade = "50 ml";
+            receita.Remedio = remedio;
+            negocio.ExibeReceita(receita);
+            
+        }
+        
+    }
+}
